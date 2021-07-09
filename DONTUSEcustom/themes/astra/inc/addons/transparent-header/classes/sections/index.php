3e5e128b235ba63b3524954b3f0572857a51e162
Gary L. Smith <85041781+garyplatformsh@users.noreply.github.com>
2021-07-09 16:44:50 -0500

    db fix hopefully

--- DONTUSEcustom/themes/astra/inc/addons/transparent-header/classes/sections/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Index file
- *
- * @package Astra Addon
- * @since Astra 1.0.0
- */
-
-/* Silence is golden, and we agree. */
--- DONTUSEcustom/themes/astra/inc/customizer/custom-controls/link/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Index file
- *
- * @package Astra
- * @since Astra 2.3.0
- */
-
-/* Silence is golden, and we agree. */
