30f723fc251627fe5470ee79c358f5caa17b0318
Paul Gilzow <paul.gilzow@platform.sh>
2024-07-19 15:44:26 -0500

    finally found a working combo of selectors to delete the post

--- .cypress/cypress/e2e/editor.cy.js
+++ .cypress/cypress/e2e/editor.cy.js
@@ -57,7 +57,7 @@ describe("Editor can log in and post", ()=>{
 		//cy.get('[aria-label="Add title"]').click().type('Hello!')
 	})
 
-  it('can delete their own posts', ()=>{
+  it.only('can delete their own posts', ()=>{
     // Auth and get session
     cy.wplogin(Cypress.env('test_user'),Cypress.env('test_user_pass'))
     // Programmatically add a post so we can delete it
@@ -67,19 +67,22 @@ describe("Editor can log in and post", ()=>{
     cy.get('#the-list').find('[data-colname="Title"]').contains(`${newPost.title} test`).should('exist')
     // this should yield the a element. we want its parent element that is a td
     cy.get('#the-list').find('[data-colname="Title"]').contains(`${newPost.title} test`).as('ourPost')
-    cy.get('@ourPost').parent('td').as('ourCell')
-
+    //cy.get('#the-list').find('[data-colname="Title"]').contains(`${newPost.title} test`).click()
+    cy.get('@ourPost').parents('td[data-colname="Title"]').as('ourCell')
+    //
+    //cy.get('@ourCell').find('div[class="row-actions"]').invoke('show')
     cy.get('@ourCell')
       .find('span[class="trash"]').contains('Trash')
-      .find('a[class="submitdelete"]').click()
+      /**
+       * @todo find out exactly what event causes this section to be visible
+       * this anchor is in a div, which has a left setting of -9XXXX. some event then removes that setting allowing it
+       * to become visible. So far I've been unable to determine the element + event that triggers it to be visible
+       * setting it to force:true for now since if you're using a screen reader you can still access it with the tab
+       * key even though it isnt visible
+        */
+      .click({force: true})
+
     cy.get('#message').should('contain','1 post moved to the Trash')
-    // //cy.closeWelcomeModal()
-    // //cy.get('.editor-post-trash').click()
-    // //3 ... menu
-    // cy.get('button[aria-label="Actions"]').click()
-    // //Move to trash "button"
-    // cy.get('#:r23:').should('contain','Move to Trash').click()
-    // cy.get('.components-flex > .is-primary').should('contain','Trash').click()
 
   })
 })
